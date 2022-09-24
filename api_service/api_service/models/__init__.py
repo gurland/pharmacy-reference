4e1fc9df497f4f23f4fe820824169494001f88b7
gurland <stas.gurland@gmail.com>
2022-09-24 17:43:57 +0300

    fix: add error handler

--- api_service/api_service/models/__init__.py
+++ api_service/api_service/models/__init__.py
@@ -1,5 +1,12 @@
+from time import sleep
+
 from .users import User
 from .summarizations import Summarization
 from .base import db
 
-db.create_tables([User, Summarization])
+for i in range(10):
+    try:
+        db.create_tables([User, Summarization])
+        break
+    except Exception:
+        sleep(1)

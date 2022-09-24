647492b2a8fab53333bb39d6c29074a9b83b67b2
teebones <ko.pasalskyi@gmail.com>
2022-09-24 22:21:51 +0300

    feat: implemented pubmed's abstract parser

--- text_summarizer_service/settings.py
+++ text_summarizer_service/settings.py
@@ -2,6 +2,9 @@ import os
 import logging
 
 SUMMARY_URL = os.getenv("SUMMARY_URL", "http://nginx:80/api/summarizations")
+PUBMED_BASE_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils"
+PUBMED_SEARCH_URL = f"{PUBMED_BASE_URL}/esearch.fcgi?db=pubmed&term={{term}}"
+PUBMED_FETCH_URL = f"{PUBMED_BASE_URL}/efetch.fcgi?db=pubmed&id={{article_id}}&rettype=xml&retmode=text"
 
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 REDIS_PORT = os.getenv("REDIS_PORT", 6379)

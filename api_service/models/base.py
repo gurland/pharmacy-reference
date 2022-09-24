4f90a8a79c6c022e54d50478b0164e8ebc1659c7
gurland <stas.gurland@gmail.com>
2022-09-24 12:44:12 +0300

    fix: import

--- api_service/models/base.py
+++ api_service/models/base.py
@@ -1,6 +1,6 @@
 from peewee import *
 from playhouse.db_url import connect
-from ..settings import DATABASE_URI
+from api_service.settings import DATABASE_URI
 
 # db = SqliteDatabase('people.db')
 db = connect(DATABASE_URI)

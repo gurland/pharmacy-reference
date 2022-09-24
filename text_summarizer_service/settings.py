01ccb6f67e7952bb2563b1f429220dfe59a7403f
gurland <stas.gurland@gmail.com>
2022-09-25 00:49:43 +0300

    fix: change log level

--- text_summarizer_service/settings.py
+++ text_summarizer_service/settings.py
@@ -11,5 +11,5 @@ REDIS_PORT = os.getenv("REDIS_PORT", 6379)
 REDIS_DB = os.getenv("REDIS_DB", 0)
 
 FORMAT = "[%(asctime)s] (%(funcName)s) <%(levelname)s>: %(message)s"
-logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+logging.basicConfig(level=logging.INFO, format=FORMAT)
 logger = logging.getLogger("summarizator")

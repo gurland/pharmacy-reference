77a85d526d174c3bd6730e9a170928a2b1f9ae8a
kiririno <>
2022-09-24 18:03:06 +0300

    feat: add summarization service

---
+++ text_summarizer_service/pubmed.py
@@ -0,0 +1,5 @@
+# Send request to PubMed API to get abstract/summary/full article/TBD
+# Parse result and prepare for neural web (remove extra punctuation)
+
+def prepare_input(input: str) -> str:
+    return input
